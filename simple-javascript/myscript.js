916da23a2494f9fbb5201d1e10eb9d9ca594013a
kelvin <edithmukoro3@gmail.com>
2023-03-07 01:19:35 +0100

    Now JS

---
+++ simple-javascript/myscript.js
@@ -0,0 +1,9 @@
+function myfunt() {
+    document.getElementById('set').innerHTML = "What's up Doc";
+}
+
+function newFunc() {
+    const cars = ['honda', 'mitsibushi', 'benz'];
+
+    document.getElementById('all').innerHTML = cars;
+}
