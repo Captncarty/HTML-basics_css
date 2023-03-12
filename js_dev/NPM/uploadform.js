310626cb95cdd804197b29aa67b1f1b601e0dcc9
kelvin <edithmukoro3@gmail.com>
2023-03-12 22:45:15 +0100

    Using node.js as a server for uploads and mailbox

--- js_dev/NPM/uploadform.js
+++ js_dev/NPM/uploadform.js
@@ -1,13 +1,20 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 //creating a HTML Form:
 http.createServer(function (req, res) {
     //parsing the Uploaded file
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, file) {
-            res.write('File Upload');
-            res.end();
+        form.parse(req, function (err, fields, files) {
+            //saving the uploaded file:
+            var oldpath = files.filetoupload.filepath;
+            var newpath = '/home/captncarty/Desktop/HTML_Basics/js_dev/NPM/' + files.filetoupload.originalFilename;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File Uploaded and moved!');
+                res.end();
+            });
         });
     } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
