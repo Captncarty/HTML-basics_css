d4522474c9b22395627a829b3b111018060e3f42
Kelvin <edithmukoro3@gmail.com>
2023-03-23 23:51:47 +0100

    new sys

--- js_dev/js-tut_ALX/map_method.js
+++ js_dev/js-tut_ALX/map_method.js
@@ -1,5 +1,5 @@
 //Map method is used to take each item in an array and pass it to a fucntion
 const timeStamp = [10, 15, 30];
-const newStamp = timeStamp.map((stamp) => stamp * 3);
+const newStamp = timeStamp.map(funcion stamp => stamp * 5);
 console.log(timeStamp);
 console.log(newStamp);
